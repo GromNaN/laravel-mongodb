fd631962ecf78160e78fef3bf4e7d3fab3f01894
Jérôme Tamarelle <jerome@tamarelle.net>
2023-07-12 11:01:27 +0200

    Move Query/Builder unit tests to a dedicated test class

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -11,12 +11,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\LazyCollection;
 use Illuminate\Testing\Assert;
 use Jenssegers\Mongodb\Collection;
-use Jenssegers\Mongodb\Connection;
 use Jenssegers\Mongodb\Query\Builder;
-use Jenssegers\Mongodb\Query\Processor;
 use Jenssegers\Mongodb\Tests\Models\Item;
 use Jenssegers\Mongodb\Tests\Models\User;
-use Mockery as m;
 use MongoDB\BSON\ObjectId;
 use MongoDB\BSON\Regex;
 use MongoDB\BSON\UTCDateTime;
@@ -32,66 +29,6 @@ class QueryBuilderTest extends TestCase
     {
         DB::collection('users')->truncate();
         DB::collection('items')->truncate();
-    }
-
-    /**
-     * @dataProvider provideQueryBuilderToMql
-     */
-    public function testMql(array $expected, \Closure $build): void
-    {
-        $builder = $build(self::getBuilder());
-        $this->assertInstanceOf(Builder::class, $builder);
-        $mql = $builder->toMql();
-
-        // Operations that return a Cursor expect a "typeMap" option.
-        if (isset($expected['find'][1])) {
-            $expected['find'][1]['typeMap'] = ['root' => 'array', 'document' => 'array'];
-        }
-        if (isset($expected['aggregate'][1])) {
-            $expected['aggregate'][1]['typeMap'] = ['root' => 'array', 'document' => 'array'];
-        }
-
-        // Compare with assertEquals because the query can contain BSON objects.
-        $this->assertEquals($expected, $mql, var_export($mql, true));
-    }
-
-    public static function provideQueryBuilderToMql(): iterable
-    {
-        /**
-         * Builder::aggregate() and Builder::count() cannot be tested because they return the result,
-         * without modifying the builder.
-         */
-        $date = new DateTimeImmutable('2016-07-12 15:30:00');
-
-        yield [
-            ['find' => [['foo' => 'bar'], []]],
-            fn (Builder $builder) => $builder->where('foo', 'bar'),
-        ];
-
-        yield [
-            ['find' => [['foo' => ['$gt' => new UTCDateTime($date)]], []]],
-            fn (Builder $builder) => $builder->where('foo', '>', $date),
-        ];
-
-        yield [
-            ['find' => [['foo' => ['$in' => ['bar', 'baz']]], []]],
-            fn (Builder $builder) => $builder->whereIn('foo', ['bar', 'baz']),
-        ];
-
-        yield [
-            ['find' => [[], ['limit' => 10, 'skip' => 5, 'projection' => ['foo' => 1, 'bar' => 1]]]],
-            fn (Builder $builder) => $builder->limit(10)->offset(5)->select('foo', 'bar'),
-        ];
-
-        yield [
-            ['distinct' => ['foo', [], []]],
-            fn (Builder $builder) => $builder->distinct('foo'),
-        ];
-
-        yield [
-            ['aggregate' => [[['$group' => ['_id' => ['foo' => '$foo'], 'foo' => ['$last' => '$foo']]]], []]],
-            fn (Builder $builder) => $builder->groupBy('foo'),
-        ];
     }
 
     public function testDeleteWithId()
@@ -892,7 +829,7 @@ class QueryBuilderTest extends TestCase
     public function testHintOptions()
     {
         DB::collection('items')->insert([
-            ['name' => 'fork',  'tags' => ['sharp', 'pointy']],
+            ['name' => 'fork', 'tags' => ['sharp', 'pointy']],
             ['name' => 'spork', 'tags' => ['sharp', 'pointy', 'round', 'bowl']],
             ['name' => 'spoon', 'tags' => ['round', 'bowl']],
         ]);
@@ -925,14 +862,5 @@ class QueryBuilderTest extends TestCase
         foreach ($results as $i => $result) {
             $this->assertEquals($data[$i]['name'], $result['name']);
         }
-    }
-
-    private static function getBuilder(): Builder
-    {
-        $connection = m::mock(Connection::class);
-        $processor = m::mock(Processor::class);
-        $connection->shouldReceive('getSession')->andReturn(null);
-
-        return new Builder($connection, $processor);
     }
 }
